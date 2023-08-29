5c57e0b084bc1a40f4dcb89eedf116038465477a
Kyle <bossertkt@gmail.com>
2023-08-29 12:33:02 -0700

    Feature 6 complete

--- brain_two_proj/todos/admin.py
+++ brain_two_proj/todos/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from todos.models import TodoList
+from todos.models import TodoList, TodoItem
 
 
 # Register your models here.
@@ -9,3 +9,8 @@ class TodoListAdmin(admin.ModelAdmin):
         "id",
         "name",
     ]
+
+
+@admin.register(TodoItem)
+class TodoItemAdmin(admin.ModelAdmin):
+    list_display = ["task", "due_date", "is_completed"]
